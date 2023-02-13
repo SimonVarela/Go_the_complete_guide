1c000b7db40c54b15a02c8611ec7c19ec94f0166
SimVarg <simon.varela@mercadolibre.com>
2023-02-13 14:50:00 -0300

    a simple start

--- 7-channels-and-routines/main.go
+++ 7-channels-and-routines/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,7 +12,7 @@ func main() {
 		"http://facebook.com",
 		"http://stackoverflow.com",
 		"http://golang.org",
-		"http://amazon.com",
+		"http://snainozama.com",
 	}
 
 	c := make(chan string)
@@ -19,14 +20,23 @@ func main() {
 	for _, link := range links {
 		go checklink(link, c)
 	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(time.Second)
+			checklink(link, c)
+		}(l)
+	}
 }
 
 func checklink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
